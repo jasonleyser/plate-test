53c7ef7496eb7bdfdef91b605f1aaec6822fa476
jasonleyser <jason.leyser@protocol.ai>
2020-11-14 15:25:27 -0700

    Index and new constants file

---
+++ common/default.js
@@ -0,0 +1,10 @@
+export const retrival = {
+  //default number of files returned
+  limit: 2,
+};
+
+export const sort = {
+  //default number of files returned
+  newest: ["created_at", "asc"],
+  oldest: ["created_at", "desc"],
+};
