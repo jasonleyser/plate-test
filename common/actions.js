b00820dcf0d378f46efaa64a4d010a5f8a067f0e
jasonleyser <jason.leyser@protocol.ai>
2020-11-17 00:20:31 -0700

    slate uploader

--- common/actions.js
+++ common/actions.js
@@ -90,3 +90,34 @@ export const addToDatabase = async (data) => {
   }
   console.log("[ fetching done ]");
 };
+//
+//
+//
+export const Upload = async ({ event, user_id, slate }) => {
+  console.log(Credentials.SLATE_API);
+  let slate_id = Credentials.SLATE_PRIVATE;
+  let api = Credentials.SLATE_API;
+  //Upload an image and insert a db query
+  let file = event.target.files[0];
+
+  const url = "https://uploads.slate.host/api/public/" + slate_id;
+  let data = new FormData();
+  data.append("data", file);
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: "Basic " + api,
+    },
+    body: data,
+  });
+  const json = await response.json();
+  // NOTE: the URL to your asset will be available in the JSON response.
+  console.log(json);
+
+  let insert = await Actions.addToDatabase({
+    user_id: user_id,
+    object_id: json.data.cid,
+    slate: slate_id,
+  });
+};
